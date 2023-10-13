5f9c3ceeb1a269b78b8e4ad1377d3a72b95ded37
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-13 16:34:01 +0200

    feat: Add dynamic message to HomeView
    - Import `ref` from Vue
    - Create a reactive variable `message` with initial value 'Hello Vue 3!'
    - Use `setTimeout` to update `message` after 2 seconds
    - Display the value of `message` in a paragraph element

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import TheWelcome from '@/components/TheWelcome.vue';
+import { ref } from 'vue';
+
+let message = ref('Hello Vue 3!');
+
+setTimeout(() => {
+  message.value = 'Hello Vue 3 from setTimeout!';
+}, 2000);
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
   </main>
 </template>
