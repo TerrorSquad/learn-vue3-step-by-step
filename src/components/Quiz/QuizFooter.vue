f20dce270c35eb3a353d0efac0f3bbb5f306c33b
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-17 19:48:47 +0200

    Refactor QuizFooter and QuizHeader components to use reactive state for the quiz name.
    - Update QuizFooter.vue to display the quiz name using reactive state.
    - Add a button in QuizFooter.vue to change the quiz name.
    - Update QuizHeader.vue to display the quiz name using reactive state.
    
    These changes improve code readability and maintainability by utilizing reactive state instead of injecting it.

--- src/components/Quiz/QuizFooter.vue
+++ src/components/Quiz/QuizFooter.vue
@@ -1,20 +1,26 @@
 <script setup>
 import QuizFooterLink from "@/components/Quiz/QuizFooterLink.vue";
-import {inject} from "vue";
+import {state as quiz} from "@/stores/quizState.js";
 
 let links = [{name: 'Google', url: "https://google.com"}, {name: 'YouTube', url: "https://youtube.com"}, {name: 'LinkedIn', url: "https://linkedin.com"}];
 
-let quiz = inject('quiz');
+
 </script>
 
 <template>
   Quiz footer
 
-  {{ quiz.name }}
+  <h5> {{ quiz.name }}</h5>
+
+  <button class="bg-blue-500 rounded-2xl py-1 px-2"
+          @click="quiz.name = 'new quiz'">Change quiz name
+  </button>
 
   <div>
     <ul class="list-none">
-      <QuizFooterLink v-for="(link, index) in links" :key="index" :link="link"/>
+      <QuizFooterLink v-for="(link, index) in links"
+                      :key="index"
+                      :link="link"/>
     </ul>
 
   </div>
--- src/components/Quiz/QuizHeader.vue
+++ src/components/Quiz/QuizHeader.vue
@@ -1,10 +1,10 @@
 <script setup>
-
+import {state as quiz} from "@/stores/quizState.js";
 </script>
 
 <template>
   <h2>
-    Quiz header
+    The quiz name: {{ quiz.name }}
   </h2>
   <p>
     Welcome to your first quiz. How do you think you'll do?
