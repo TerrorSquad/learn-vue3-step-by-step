4d3dc32c918875ec670e42bd891d5c9e3910dd7a
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-17 23:10:32 +0200

    Refactor Modal component transitions and styles
    - Updated the transition classes for entering and leaving the modal
    - Added scoped styles to the Modal component

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,22 +5,29 @@ defineProps({
 </script>
 
 <template>
-  <div v-if="show"
-       class="modal-mask">
-    <div class="modal-container">
-      <div class="mb-2">
-        <slot>default body</slot>
-      </div>
+  <Transition enter-active-class="transition duration-300"
+              enter-from-class="opacity-0 scale-125"
+              enter-to-class="opacity-100"
+              leave-active-class="transition duration-300"
+              leave-to-class="opacity-0 scale-125"
+  >
+    <div v-if="show"
+         class="modal-mask">
+      <div class="modal-container">
+        <div class="mb-2">
+          <slot>default body</slot>
+        </div>
 
-      <footer class="border-t border-blue-100 pt-2">
-        <slot name="footer">
-          <button class="rounded-xl bg-blue-500 hover:bg-blue-600 py-1 px-2 text-white"
-                  @click="$emit('close')">Close
-          </button>
-        </slot>
-      </footer>
+        <footer class="border-t border-blue-100 pt-2">
+          <slot name="footer">
+            <button class="rounded-xl bg-blue-500 hover:bg-blue-600 py-1 px-2 text-white"
+                    @click="$emit('close')">Close
+            </button>
+          </slot>
+        </footer>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <style scoped>
